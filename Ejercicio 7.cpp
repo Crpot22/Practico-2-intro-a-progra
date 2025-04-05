3f57729d7cb208ca742b8367af120a6b03896347
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-05 00:24:21 -0400

    Create Ejercicio 7.cpp

---
+++ Ejercicio 7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    string libro_1, libro_2, libro_3 ;
+    
+    getline(cin, libro_1) ;
+    getline(cin, libro_2) ;
+    getline(cin, libro_3) ;
+    
+    cout<<libro_3<<endl;
+    cout<<libro_2<<endl;
+    cout<<libro_1<<endl;
+
+    
+    return 0;
+}
