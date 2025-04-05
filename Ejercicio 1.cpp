3d42dd41ffb778935245f9535e86d4e5982ed704
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-05 00:06:39 -0400

    Create Ejercicio 1.cpp

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    double x ;
+    double y ;
+    cout<<"Ingrese un valor para x"<<endl;
+    cin>>x;
+    cout<< "Ingrese un valor para y"<<endl;
+    cin>>y;
+    int suma= x + y;
+    cout<<"La suma representada en numero enteros es de: "<<suma<<endl;
+
+    
+    return 0;
+}
