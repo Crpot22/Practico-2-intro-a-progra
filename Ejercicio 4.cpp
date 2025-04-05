5b94c0bb03d01285900ba89da24ac25aa00884a3
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-05 00:11:50 -0400

    Create Ejercicio 4.cpp

---
+++ Ejercicio 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    int x ;
+    int y ;
+    cin >> x >> y;
+    int z = 1;
+    int suma = x+y+z;
+    cout<<"Your answer: "<<suma<<endl;
+
+
+    
+    return 0;
+}
