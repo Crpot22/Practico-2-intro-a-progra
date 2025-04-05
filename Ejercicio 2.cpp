2ab0ed9e26085997407e54ae524e5b78e45e5828
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-05 00:09:30 -0400

    Create Ejercicio 2.cpp

---
+++ Ejercicio 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+  
+    double x ;
+    double y ;
+    cout<<"Ingrese un valor para x"<<endl;
+    cin>>x;
+    cout<< "Ingrese un valor para y"<<endl;
+    cin>>y;
+    double division = x / y ;
+    cout<<"El cociente es de: "<<division<<endl;
+
+    
+    return 0;
+}
