bccc4dec3526d7c8a8a4c9192c061b973b0941e8
ChrisJMT007 <cmachicadojosue@gmail.com>
2025-04-05 22:34:57 -0400

    Create Ejerjcio 8.cpp

---
+++ Ejerjcio 8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string a, b, c;
+    cin >> a >> b >> c;
+
+    if (b < a) {
+        a = b;
+    }
+    if (c < menor) {
+        a= c;
+    }
+
+    cout << a << endl;
+
+    return 0;
+}
